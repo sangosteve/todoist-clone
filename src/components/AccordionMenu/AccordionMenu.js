a9a441a60d280d20ea0293444c8d4957f8b97e31
sangosteve <stevesango9@gmail.com>
2020-12-22 10:07:30 +0200

    added the caret rotation animation to the link element

--- src/components/AccordionMenu/AccordionMenu.js
+++ src/components/AccordionMenu/AccordionMenu.js
@@ -1,20 +1,25 @@
 import React, { useState, useContext } from "react";
 import { FiChevronRight} from "react-icons/fi";
+import { FiPlus} from "react-icons/fi";
 import { AccordionMenuContext } from "./AccordionMenuContext";
 import './AccordionMenu.css';
 
 function AccordionMenu() {
-  const [accordion,setAccordion] = useContext(AccordionMenuContext);  
+  const {accordion,iconCaret} = useContext(AccordionMenuContext);
+  const[accordionValue,setAccordionValue] = accordion;
+  const[iconCaretValue,setIconCaretValue] = iconCaret;
+
   function ToggleAccordion(){
-      setAccordion(!accordion);
+      setAccordionValue(!accordionValue);
+      setIconCaretValue(!iconCaretValue);
   }
   return (
     <div id="AccordionMenu">
       <ul id="accordion-widget">
         <li>
-          <div className="link" onClick={ToggleAccordion}><FiChevronRight/>Projects</div>
+          <div className="link" onClick={ToggleAccordion}><FiChevronRight className={iconCaretValue?"icon-caret caret-down":"icon-caret"}/>Projects <FiPlus className="icon-plus"/></div>
 
-          <ul className={accordion?"accordion-menu active":"accordion-menu"}>
+          <ul className={accordionValue?"accordion-menu active":"accordion-menu"}>
             <li>
               <a>Html</a>
             </li>
--- src/components/AccordionMenu/AccordionMenuContext.js
+++ src/components/AccordionMenu/AccordionMenuContext.js
@@ -2,8 +2,9 @@ import React,{createContext,useState} from 'react';
 export const AccordionMenuContext = createContext();
 export const AccordionMenuProvider = props=>{
     const [accordion,setAccordion] = useState(false);
+    const [iconCaret,setIconCaret] = useState(false);
     return(
-        <AccordionMenuContext.Provider value={[accordion,setAccordion]}>
+        <AccordionMenuContext.Provider value={{accordion:[accordion,setAccordion],iconCaret:[iconCaret,setIconCaret]}}>
             {props.children}
         </AccordionMenuContext.Provider>
     )
